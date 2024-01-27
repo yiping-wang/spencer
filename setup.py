22a5de9cb50dfbda1536d0dd854c3eac2af7b6ab
Yiping Wang <yiping.wang@gmx.com>
2024-01-27 17:55:20 -0500

    package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spencer",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "openai==1.10.0",
+        "tiktoken==0.5.2",
+        "pyarrow==15.0.0",
+    ],
+    # Optional metadata
+    author="Yiping Wang",
+    author_email="yiping.wang@gmx.com",
+    description="A short description of the project.",
+    keywords="example project",
+    url="https://example.com/project-url",  # Project home page
+)
